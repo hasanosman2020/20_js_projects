3550e9254dc8dcb52029523c62a4be8aa5ce48a6
hasanosman2020 <hasan79.hoa@gmail.com>
2022-07-01 04:07:32 +0100

    Quote Generator: populate the UI with a new quote and author name when user refreshes page

--- quote_generator/app.js
+++ quote_generator/app.js
@@ -1,10 +1,18 @@
+const quoteContainer = document.getElementById('quote_container')
+const quoteText = document.getElementById('quote')
+const authorName = document.getElementById('author')
+const twitterButton = document.getElementById('twotter')
+const newQuoteButton = document.getElementById('new_quote')
+
 let apiQuotes = []
 
 //Show new quote
 function newQuote () {
   //Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
-  console.log(quote)
+  //console.log(quote)
+  authorName.textContent = quote.author
+  quoteText.textContent = quote.text
 }
 
 //Get quotes from API
@@ -23,3 +31,4 @@ async function getQuotes () {
 
 //On Load
 getQuotes()
+//newQuote()
