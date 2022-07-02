89c3a94e0503b0771e2ef32cdab31f8cae0b4fde
hasanosman2020 <hasan79.hoa@gmail.com>
2022-07-02 14:18:52 +0100

    feat: Add spinning loader
    Create loading() and completeLoading() functions to control when the spinning loader appears

--- quote_generator/app.js
+++ quote_generator/app.js
@@ -3,30 +3,47 @@ const quoteText = document.getElementById('quote')
 const authorName = document.getElementById('author')
 const twitterButton = document.getElementById('twitter')
 const newQuoteButton = document.getElementById('new_quote')
+const loader = document.getElementById('loader')
 
 let apiQuotes = []
 
+//Show loading
+function loading () {
+  loader.hidden = false
+  quoteContainer.hidden = true
+}
+//Show complete loading
+function completeLoading () {
+  quoteContainer.hidden = false
+  loader.hidden = true
+}
+
 //Show new quote
 function newQuote () {
+  loading()
+
   //Pick a random quote from apiQuotes array
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
   console.log(quote)
   authorName.textContent = quote.author
   quoteText.textContent = quote.text
 
+  //Check if author name is null and replace it with 'Anonymous'
   if (quote.author === null) {
     authorName.textContent = 'Anonymous'
   }
-
+  //Check length of quote to determine styling
   if (quote.text.length > 80) {
     quoteText.classList.add('long_quote')
   } else {
     quoteText.classList.remove('long_quote')
   }
+  completeLoading()
 }
 
 //Get quotes from API
 async function getQuotes () {
+  loading()
   const apiUrl = 'https://type.fit/api/quotes'
   try {
     const response = await fetch(apiUrl)
@@ -52,3 +69,4 @@ twitterButton.addEventListener('click', tweetQuote)
 //On Load
 getQuotes()
 //newQuote()
+//loading()
