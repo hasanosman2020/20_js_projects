42f8fef24a6c2d405470731057b9789773b8250b
hasanosman2020 <hasan79.hoa@gmail.com>
2022-08-19 18:59:46 +0100

    feat: Implement infinite scroll functionality. Complete project

--- infinite_scroll/script.js
+++ infinite_scroll/script.js
@@ -1,11 +1,25 @@
 const imageContainer = document.getElementById('image_container')
 const loader = document.getElementById('loader')
 
+let imagesLoaded = 0
+let totalImages = 0
+let ready = false
 let photosArray = []
 
 const myKey = config.MY_KEY
-const count = 10
+const count = 30
 const apiUrl = `https://api.unsplash.com/photos/random?client_id=${myKey}&count=${count}`
+
+function imageLoaded () {
+  imagesLoaded++
+  console.log(imagesLoaded)
+
+  if (imagesLoaded === totalImages) {
+    ready = true
+    loader.hidden = true
+    console.log('ready =', ready)
+  }
+}
 
 //Helper function to set attributes on DOM elements
 function setAttributes (element, attributes) {
@@ -16,6 +30,10 @@ function setAttributes (element, attributes) {
 
 //Create elements for links and photos and add to DOM
 function displayPhotos () {
+  imagesLoaded = 0
+  totalImages = photosArray.length
+  console.log('total images', totalImages)
+
   //Run function for each object in photosArrsy
   photosArray.forEach(photo => {
     //Create <a> to link to Unsplash
@@ -38,6 +56,9 @@ function displayPhotos () {
       title: photo.alt_description
     })
 
+    //Event listener to check when each image has finished loading
+    img.addEventListener('load', imageLoaded)
+
     //Put <img> inside <;> then put both inside imageContainer
     item.appendChild(img)
     imageContainer.appendChild(item)
@@ -58,5 +79,28 @@ async function getPhotos () {
   }
 }
 
+//Check to see if scrolling near bottom of page
+window.addEventListener('scroll', e => {
+  //console.log('scrolled')
+  //console.log('event.target:', e.target)
+  //console.log('event.currentTarget:', e.currentTarget)
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    //console.log('window.innerHeight:', window.innerHeight)
+    //console.log('window.scrollY:', window.scrollY)
+    //console.log(
+    //'window.innerHeight + window.scrollY:',
+    //window.innerHeight + window.scrollY
+    //)
+    //console.log(
+    //'document.body.offsetHeight - 1000:',
+    //document.body.offsetHeight - 1000
+    //)
+
+    getPhotos()
+  }
+})
 //On page load
 getPhotos()
